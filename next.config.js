f2efcd90f09287483b92dcf3876566bca25e12c3
“ptrenda” <treendaa@gmail.com>
2023-06-17 19:54:12 +0200

    adjust next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-};
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = '/'
+let basePath = ''
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `https://www.24campercamp.pl/`
+  basePath = `https://www.24campercamp.pl/${repo}`
+}
+
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix,
+  basePath: basePath
+};
 module.exports = nextConfig;
